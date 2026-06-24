5646822cd969103297fbc3ae74f23f73ee60faf0
sage2002 <gubai17092002@gmail.com>
2026-06-24 17:10:42 +0530

    Update authentication and implement Clerk SignUp component

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,7 +26,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-
+  password: {
+    type: String,
+    required: false,
+    select: false
+  },
   role: {
     type: String,
     enum: ['superadmin', 'admin', 'manager', 'member'],
@@ -35,9 +40,23 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: true
   },
-  lastLogin: {
+  lastActiveAt: {
     type: Date
   }
 }, { timestamps: true });
+
+// Encrypt password using bcryptjs on save
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare password helper
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model('User', UserSchema);
